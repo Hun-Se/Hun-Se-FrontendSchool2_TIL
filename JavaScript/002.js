e54b04b231cefe0043620d9502ea308717ec065b
Hun-Se <kim72840@gamil.com>
2022-04-27 20:16:15 +0900

    테일윈드,자바스크립트추가

---
+++ JavaScript/002.js
@@ -0,0 +1,5 @@
+document.getElementById("one").innerHTML = "hello <strong>world document</strong>";
+console.log("hello world console");
+window.alert("hello world alert");
+console.error("hello world error");
+document.write("hello!!");
