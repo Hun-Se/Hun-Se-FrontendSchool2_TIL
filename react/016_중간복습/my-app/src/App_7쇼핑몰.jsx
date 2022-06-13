8f247d8b55ec117f0b539dab4bd1023a7f099782
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-06-13 17:30:39 +0900

    Add files via upload

---
+++ react/016_중간복습/my-app/src/App_7쇼핑몰.jsx
@@ -0,0 +1,13 @@
+import HomPage from './pages/Homepage/HomPage';
+
+
+function App() {
+    // console.log(data)
+    return (
+        <div>
+          <HomPage/>
+        </div>
+    );
+}
+
+export default App;
