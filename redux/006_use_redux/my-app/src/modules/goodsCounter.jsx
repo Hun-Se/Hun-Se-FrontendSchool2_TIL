0d97da98e608f0ca6a428bc45df1e7667327589e
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-06-20 23:12:13 +0900

    Add files via upload

---
+++ redux/006_use_redux/my-app/src/modules/goodsCounter.jsx
@@ -0,0 +1,40 @@
+// 리덕스의 리듀서 만들기!
+// 액션 생성함수
+export const addNumber = () => {
+  return { type: "ADD" };
+};
+
+export const substractNumber = () => {
+  return { type: "SUBSTRACT" };
+};
+
+// 초기 값
+const initialState = {
+  stock: 10,
+  goods: 1
+};
+
+// 리듀서
+const goodsReducer = (state = initialState, action) => {
+  console.log(action)
+  console.log(action.type)
+  console.log(state)
+  switch (action.type) {
+      case "ADD":
+          return {
+              ...state,
+              stock : state.stock - 1,
+              goods : state.goods + 1,
+          }
+      case "SUBSTRACT":
+          return {
+              ...state,
+              stock : state.stock + 1,
+              goods : state.goods - 1,
+          }
+      default:
+          return state;
+  }
+};
+
+export default goodsReducer;
